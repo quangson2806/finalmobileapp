8bc26c7ab7a37fe235925a406ea622d07eb9d4e3
quangson2806 <vipboypro01@gmail.com>
2023-01-03 01:09:50 +0700

    File

---
+++ FinalAppMobile/app/src/main/java/com/edu/usth/finalappmobile/Home_Page_Screen.java
@@ -0,0 +1,14 @@
+package com.edu.usth.finalappmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_Page_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page_screen);
+    }
+}
